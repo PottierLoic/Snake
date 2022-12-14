b18bd35577e513b6712cde38d6935e02aada31c6
PottierLoic <loic.pottier@live.fr>
2022-12-14 19:43:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-# SnakeAI
+# Snake
 # Author : Loïc Pottier
 # Creation date : 14/12/2022
 
 # IMPORTS
-
 from tkinter import *
 import random
 
-# CONSTANS
-
+# CONSTANTS
 GAME_WIDTH = 1000
 GAME_HEIGHT = 700
-SPEED = 40
+SPEED = 50
 SPACE_SIZE = 25
 BODY_PARTS = 3
 SNAKE_COLOR = "#00CCCC"
 FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
-
 class Snake:
-    
+
     def __init__(self):
         self.bodySize = BODY_PARTS
         self.coordinates = []
@@ -34,9 +31,8 @@ class Snake:
             self.squares.append(square)
 
 class Food:
-    
-    def __init__(self):
 
+    def __init__(self):
         x = random.randint(0, (GAME_WIDTH/SPACE_SIZE-1)) * SPACE_SIZE
         y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE-1)) * SPACE_SIZE
 
@@ -45,7 +41,7 @@ class Food:
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 def nextTurn(snake, food):
-    
+
     x, y = snake.coordinates[0]
 
     if direction == "up":
@@ -113,15 +109,13 @@ def gameOver():
     canvas.delete(ALL)
     canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2, font=("consolas", 70), text="GAME OVER", fill="red", tag="gameover")
 
+# Main window creation
 window = Tk()
 window.title("Snake AI")
 window.resizable(False, False)
 
 score = 0
 direction = "down"
-
-
-
 
 label = Label(window, text="Score : {}".format(score), font=("consolas", 40))
 label.pack()
@@ -131,18 +125,17 @@ canvas.pack()
 
 window.update()
 
-
 windowWidth = window.winfo_width()
 windowHeight = window.winfo_height()
 screenWidth = window.winfo_screenwidth()
 screenHeight = window.winfo_screenheight()
-
 
 x = int((screenWidth/2) - (windowWidth/2))
 y = int((screenHeight/2) - (windowHeight/2))
 
 window.geometry(f"{windowWidth}x{windowHeight}+{x}+{y}")
 
+# Key bindings
 window.bind("<Left>", lambda event: changeDirection("left"))
 window.bind("<Right>", lambda event: changeDirection("right"))
 window.bind("<Up>", lambda event: changeDirection("up"))
